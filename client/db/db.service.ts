f0f97350e557d031d3cfb6a5346f334fe48ab109
mw_clickbar <mw@clickbar.dev>
2020-12-11 15:07:16 +0100

    added replication setup to client

--- client/db/db.service.ts
+++ client/db/db.service.ts
@@ -8,12 +8,17 @@ import { RxDBLeaderElectionPlugin } from 'rxdb/plugins/leader-election'
 
 import { RxDBReplicationPlugin } from 'rxdb/plugins/replication'
 
-import { RxDBReplicationGraphQLPlugin } from 'rxdb/plugins/replication-graphql'
+import {
+  pullQueryBuilderFromRxSchema,
+  pushQueryBuilderFromRxSchema,
+  RxDBReplicationGraphQLPlugin,
+  syncGraphQL,
+} from 'rxdb/plugins/replication-graphql'
 
 import * as PouchdbAdapterIdb from 'pouchdb-adapter-idb'
 
 import { MyDatabase, Collections } from './types'
-import entrySchema from './schema/entry.schema'
+import entrySchema from './schema/entry'
 
 if (process.env.NODE_ENV === 'development') {
   addRxPlugin(RxDBDevModePlugin)
@@ -25,6 +30,27 @@ addRxPlugin(PouchdbAdapterIdb)
 addRxPlugin(RxDBReplicationGraphQLPlugin)
 
 const syncURL = 'http://localhost:4000/graphql'
+const batchSize = 5
+export const graphQLGenerationInput = {
+  entry: {
+    schema: entrySchema,
+    feedKeys: ['id', 'updatedAt'],
+    deletedFlag: 'deleted',
+    subscriptionParams: {
+      token: 'String!',
+    },
+  },
+}
+
+const pullQueryBuilder = pullQueryBuilderFromRxSchema(
+  'entry',
+  graphQLGenerationInput.entry,
+  batchSize
+)
+const pushQueryBuilder = pushQueryBuilderFromRxSchema(
+  'entry',
+  graphQLGenerationInput.entry
+)
 
 async function _create(): Promise<MyDatabase> {
   console.log('DatabaseService: creating database..')
@@ -35,6 +61,20 @@ async function _create(): Promise<MyDatabase> {
 
   await db.addCollections({ entries: { schema: entrySchema } })
 
+  const replication = db.collections.entries.syncGraphQL({
+    url: syncURL,
+    push: {
+      batchSize,
+      queryBuilder: pushQueryBuilder,
+    },
+    pull: {
+      queryBuilder: pullQueryBuilder,
+    },
+    live: true,
+    liveInterval: 1000 * 60 * 10,
+    deletedFlag: 'deleted',
+  })
+  // await replication.awaitInitialReplication()
   return db
 }
 
