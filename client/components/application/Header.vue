a8262418723f06e636e4d7551335c970937d7b9f
Venturh <venturh94@gmail.com>
2020-12-22 12:19:43 +0100

    added input functionality

--- client/components/application/Header.vue
+++ client/components/application/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-between space-x-2">
+  <div class="flex items-center justify-between">
     <div class="lg:hidden">
       <IconOnlyButton @click="$emit('sidebartoggle')">
         <MenuIcon class="stroke-current text-brand" />
@@ -28,6 +28,8 @@
 </template>
 
 <script lang="ts">
+import DatabaseService from '@/db/db'
+import { EntryInput } from '@/generated/graphql'
 import { defineComponent, ref, watch } from '@nuxtjs/composition-api'
 import { MenuIcon, PlusIcon } from 'vue-feather-icons'
 export default defineComponent({
@@ -52,8 +54,37 @@ export default defineComponent({
       }
     )
 
-    function add(data: string) {
-      console.log('🚀 ~ file: Header.vue ~ line 33 ~ add ~ data', data)
+    async function add(data: string) {
+      const split = data.split(' ')
+      let categories = split.filter((s) => s.includes('#'))
+      if (categories) {
+        categories = categories.map((s) => {
+          split.splice(split.indexOf(s), 1)
+          return s.substring(1)
+        })
+      }
+
+      const contentUrl = data.match(/\b(https?:\/\/.*?\.[a-z]{2,4}\/[^\s]*\b)/g)
+      if (contentUrl) split.splice(split.indexOf(contentUrl[0], 1))
+      const contentType = contentUrl === null ? 'Note' : 'Link'
+
+      const id = require('bson-objectid')
+      const entry: EntryInput = {
+        id: id().str,
+        contentText: split.join(' '),
+        contentType,
+        contentUrl: contentUrl === null ? '' : contentUrl![0],
+        // contentUrl:'https://open.spotify.com/track/1J2OlTIPluzOmf3RX8eKhT?si=vnZacwXhTEGC4Hbb95u2mg',
+        categories,
+        hashedKey: 'hashed',
+        calendarDate: Date().toString(),
+        processing: false,
+        updatedAt: Date.now().toString(),
+      }
+      const db = await DatabaseService.get()
+      await db.entries.insert(entry)
+      inputType.value = 'search'
+      input.value = ''
     }
 
     return { input, add, inputType }
--- client/pages/entries.vue
+++ client/pages/entries.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="flex flex-col-reverse space-x-6 lg:flex-row lg:justify-between">
     <div>
-      <p v-if="!loading">Welcome back {{ me.email }}</p>
-      <button @click="test">INSERT</button>
       <h1 v-if="awaitReplication">Waiting for awaitReplication</h1>
-      <div v-if="!awaitReplication" class="space-y-2">
+      <div v-else class="space-y-2">
         <!-- Todo: v-bind="entry" throws error -->
         <BaseEntry
           v-for="(entry, index) in entries"
@@ -31,36 +29,13 @@
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from '@nuxtjs/composition-api'
-import { useResult } from '@vue/apollo-composable'
-import { EntryInput, useMeQuery } from '@/generated/graphql'
 import DatabaseService from '@/db/db'
 
 export default defineComponent({
   middleware: ['authenticated'],
   setup() {
-    const { result, loading } = useMeQuery()
-
-    const me = useResult(result, null, (data) => data.me)
     const entries = ref()
     const awaitReplication = ref(true)
-
-    async function test() {
-      const db = await DatabaseService.get()
-      const id = require('bson-objectid')
-      const obj: EntryInput = {
-        id: id().str,
-        contentText: 'Meine Playlist zu der ich code',
-        contentType: 'Link',
-        contentUrl: 'https://www.youtube.com/watch?v=5qap5aO4i9A',
-        // contentUrl:'https://open.spotify.com/track/1J2OlTIPluzOmf3RX8eKhT?si=vnZacwXhTEGC4Hbb95u2mg',
-        categories: ['Youtube', 'Test'],
-        hashedKey: 'hashed',
-        calendarDate: Date().toString(),
-        processing: false,
-        updatedAt: Date.now().toString(),
-      }
-      await db.entries.insert(obj)
-    }
 
     onMounted(async () => {
       awaitReplication.value = true
@@ -72,7 +47,7 @@ export default defineComponent({
       awaitReplication.value = false
     })
 
-    return { me, loading, entries, test, awaitReplication }
+    return { entries, awaitReplication }
   },
 })
 </script>
